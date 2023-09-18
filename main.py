52bc3dcac45fc89a4235068e4a85ea3acedb7e14
NICHOLAS-1217 <nicholastan1217@gmail.com>
2023-09-19 00:45:52 +0800

    testing

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
 import requests
-from bs4 import BeautifulSoup
 
-#making get request
-r = requests.get('https://www.geeksforgeeks.org/python-programming-language/')
-
-#show reponse
-print(r)
-
-soup = BeautifulSoup(r.content, 'html.parser')
-#show html
-print(soup.prettify())
-#show title tag
-print(soup.title)
-#show tag name
-print(soup.title.name)
-#show tag parent name
-print(soup.title.parent.name)
+requests.get('https://blue-horizon-deepdive.netlify.app/')
