f1ac10aa668cfc617fff4f992c1379bf7b4668ae
NICHOLAS-1217 <nicholastan1217@gmail.com>
2023-09-19 20:24:14 +0800

    requests basic

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 import requests
 
-requests.get('https://blue-horizon-deepdive.netlify.app/')
+headers = {
+    'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
+}
+
+kw = {
+    'wd' : 'python'
+}
+
+keyword = {
+    'keyword' : 'python'
+}
+
+url = 'https://www.facebook.com/'
+
+data = {
+    'wd' : 'python'
+}
+
+response = requests.get(url = url, headers = headers)
+
+response.encoding = 'utf-8'
+
+# print(response.content)
+print(response.text)
+print(response)
+print(response.url)
