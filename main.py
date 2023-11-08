6511c6948630bcb411bc6912a7dbc5e8f7e68c93
NICHOLAS-1217 <nicholastan1217@gmail.com>
2023-11-08 13:43:07 +0800

    login to camsys

--- main.py
+++ main.py
@@ -4,22 +4,19 @@ headers = {
     'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
 }
 
-url = 'https://www.facebook.com/login/?privacy_mutation_token=eyJ0eXBlIjowLCJjcmVhdGlvbl90aW1lIjoxNjk1MTI2OTI2LCJjYWxsc2l0ZV9pZCI6MzgxMjI5MDc5NTc1OTQ2fQ%3D%3D'
+url = 'https://camsys.mmu.edu.my/psp/csprd/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_CART.GBL?PORTALPARAM_PTCNAV=HC_SSR_SSENRL_CART_GBL&EOPP.SCNode=HRMS&EOPP.SCPortal=EMPLOYEE&EOPP.SCName=CO_EMPLOYEE_SELF_SERVICE&EOPP.SCLabel=Enrollment&EOPP.SCFName=HCCC_ENROLLMENT&EOPP.SCSecondary=true&EOPP.SCPTfname=HCCC_ENROLLMENT&FolderPath=PORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.N_NEW_ACADEMICS.N_NEW_CRSENRL.HCCC_ENROLLMENT.HC_SSR_SSENRL_CART_GBL&IsFolder=false'
 
-data = {
-    'jazoest': '2951',
-    'lsd' : 'AVpia6DT_lM',
-    'email': '0177183063',
-    'login_source': 'comet_headerless_login',
-    'next': '',
-    'encpass': '#PWD_BROWSER:5:1695126932:ASlQAB7cw32vUvvul+cFXlrJlSIuLLzaN9ap88lIDOP4qRhi0X5eRA3fnwwyYYPWFPpco7+6zGSyrNTdKt54cir1CabCFNxakCbmCTMPIl2N1bqYQCLC9ADTTIK7UufXFzR9wgnFldnghbpKbiMFU78aUA=='
+payload = {
+    'cmd' : 'login',
+    'languageCd' : 'ENG',
+    'timezoneOffset' : '-480',
+    'userid' : '1211104786',
+    'pwd' : 'Tanzhixuan_1217'
 }
 
-response = requests.post(url = url, headers = headers, data = data)
+r = requests.post(url = url, headers = headers, data=payload)
+r.encoding = 'utf-8'
 
-response.encoding = 'utf-8'
-
-# print(response.content)
-print(response.text)
-print(response)
-print(response.url)
+print(r.text)
+print(r)
+print(r.url)
